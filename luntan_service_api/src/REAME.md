d8fbe7a73e8180684f3929cb0b6aa6a50922b3be
wangjun <962903632@qq.com>
2019-11-30 09:07:33 +0800

    加了一些说明

---
+++ luntan_service_api/src/REAME.md
@@ -0,0 +1,4 @@
+#接口说明
+* ###ITopicService 贴子服务接口
+* ###IUserService 用户服务接口
+    * Integer register(User user) --- 注册
--- luntan_service_api/src/main/java/com/qf/service/ITopicService.java
+++ luntan_service_api/src/main/java/com/qf/service/ITopicService.java
@@ -1,7 +1,7 @@
 package com.qf.service;
 
 /**
- * 论坛服务接口
+ * 帖子服务接口
  * @author wangjun
  * @name wwluntan
  * @time 2019/11/29 21:57
